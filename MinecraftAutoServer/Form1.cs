e827695f7ea83d4b5e84c119048149a656457a26
Ammar Hakim <ammarhakim57@gmail.com>
2022-10-18 00:28:16 -0400

    Textbox focus

--- MinecraftAutoServer/Form1.cs
+++ MinecraftAutoServer/Form1.cs
@@ -36,5 +36,17 @@ namespace MinecraftAutoServer
         {
 
         }
-    }
+
+        // FOCUSSED TEXTBOX TEMPORARY CODE
+        private void TextBox_Focus(object sender, RoutedEventArgs e)
+        {
+            if (!hasBeenClicked)
+            {
+                TextBox box = sender as TextBox;
+                box.Text = String.Empty;
+                hasBeenClicked = true;
+            }
+
+
+        }
 }
